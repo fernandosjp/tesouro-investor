c7743d4881747cd197f3dfdc80763573970fc64a
fernandosjp <fernandosjp@gmail.com>
2016-01-13 01:02:29 -0200

    Add click to required modules

--- setup.py
+++ setup.py
@@ -78,7 +78,8 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['urllib2',
                       'pandas',
-                      'email'],
+                      'email',
+                      'click'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
